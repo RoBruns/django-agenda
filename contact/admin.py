dcd46af901f079451370bc79c26c9d773ba653e8
Rodrigo <rodrigobrunsfh@gmail.com>
2024-01-02 20:05:52 -0300

    criando Model Category e FK link dele

--- contact/admin.py
+++ contact/admin.py
@@ -5,10 +5,16 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'created_date' # noqa
+    list_display = 'id', 'category', 'first_name', 'last_name', 'phone', 'email', 'created_date', # noqa
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name', 'phone',
     list_per_page = 10
     list_max_show_all = 200
     list_display_links = 'id', 'first_name'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,13 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -13,6 +20,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
