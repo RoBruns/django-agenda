fc4c6e53f3d6c606710e2e4fc8a6b1651b55617d
Rodrigo <rodrigobrunsfh@gmail.com>
2024-01-02 19:13:13 -0300

    Create imageField e configurando o MEDIA_URL e MEDIA_ROOT no settings.py

--- contact/models.py
+++ contact/models.py
@@ -11,6 +11,8 @@ class Contact(models.Model):
     email = models.EmailField(blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
