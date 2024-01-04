14041e762821827130c15e8779b2066e711c59d2
Rodrigo <rodrigobrunsfh@gmail.com>
2024-01-04 17:48:21 -0300

    filtrando valores no search usando Q e OR

--- contact/urls.py
+++ contact/urls.py
@@ -8,4 +8,5 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -10,6 +11,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos -'
     }
 
     return render(
@@ -19,11 +21,41 @@ def index(request):
     )
 
 
-def contact(request, contact_id):   
+def search(request):
+    search_value = request.GET.get('q', "").strip()
+
+    if not search_value:
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(id__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': f'Search: {search_value} -'
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id,):
     sigle_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
     context = {
         'contact': sigle_contact,
+        'site_title': f'{sigle_contact.first_name} {sigle_contact.last_name} -'
     }
 
     return render(
