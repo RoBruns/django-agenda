37eb8e16fe8c217f5d74b22d0007a806bf815a5c
Rodrigo <rodrigobrunsfh@gmail.com>
2024-01-06 17:30:33 -0300

    fazendo o html e css do create.html

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,8 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
+
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
